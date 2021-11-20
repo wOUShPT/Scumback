c362102296505a1c1cd3955adaee921fb139c621
wOUShPT <61854303+wOUShPT@users.noreply.github.com>
2021-11-20 15:57:01 +0000

    Lobby / Inputs and Boat

---
+++ Assets/Scripts/BoatController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BoatController : MonoBehaviour
+{
+    private PlayerInputManagerHandler inputManagerHandler;
+
+    private List<PlayerInputController> _playersControllers;
+    [SerializeField] private Transform boatTransform;
+
+    [SerializeField] private float speed;
+
+    private Vector2 _startPosition;
+    private Vector2 _currentPosition;
+
+    private void Start()
+    {
+        inputManagerHandler = FindObjectOfType<PlayerInputManagerHandler>();
+        _startPosition = boatTransform.position;
+        _currentPosition = _startPosition;
+        _playersControllers = FindObjectsOfType<PlayerInputController>().ToList();
+        foreach (var player in _playersControllers)
+        {
+            Debug.Log(player.PlayerIndex);
+        }
+    }
+
+    void Update()
+    {
+        foreach (var playerController in _playersControllers)
+        {
+            //Debug.Log(playerController.input.action);
+            if (playerController.input.action > 0 && playerController.PlayerIndex == 0)
+            {
+                _currentPosition.x -= speed * Time.deltaTime;
+            }
+
+            if (playerController.input.action > 0 && playerController.PlayerIndex == 1)
+            {
+                _currentPosition.x += speed * Time.deltaTime;
+            }
+        }
+        boatTransform.position = _currentPosition;
+    }
+}
+
+    
---
+++ Assets/Scripts/PlayerInputManagerHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerInputManagerHandler : MonoBehaviour
+{
+    private PlayerInput inputDummy;
+    private List<Gamepad> gamepads;
+
+    public void StartGame()
+    {
+        if (PlayerInputManager.instance.playerCount == 2)
+        {
+            SceneManager.LoadScene("Gustavo");
+        }
+    }
+}
