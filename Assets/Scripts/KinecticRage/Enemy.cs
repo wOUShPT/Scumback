ec913297e7ccc3d1ad361e99e562c25d0e4092ee
barbosayyy <72970256+barbosayyy@users.noreply.github.com>
2021-11-21 04:24:21 +0000

    KinecticRage 1

--- Assets/Scripts/KinecticRage/Enemy.cs
+++ Assets/Scripts/KinecticRage/Enemy.cs
@@ -1,83 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Enemy : MonoBehaviour
 {
     public GameObject rightFist;
     public GameObject leftFist;
-
-    public Vector3 rightFistX;
-    public Vector3 rightFistY;
-
-    public Vector3 leftFistX;
-    public Vector3 leftFistY;
+    public GameObject canAttackSprite;
 
     public LevelManager levelManager;
-    public bool canAttack;
-    public bool leftAttack;
+
+    public bool isAttacking;
+    public bool hasAttacked;
+    public bool attackHit;
+
     public float randomValue;
     public float attackTimer;
 
+    public int swingTime;
+
+    private List<PlayerInputController> _playersControllers;
+
+    public Collider2D rightFistCol;
+    public Collider2D leftFistCol;
+
     void Start()
     {
+        _playersControllers = FindObjectsOfType<PlayerInputController>().ToList();
         levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
-
-        rightFistX = new Vector3(rightFist.transform.position.x,0,0);
-        rightFistY = new Vector3(0, rightFist.transform.position.y, 0);
-
-        leftFistX = new Vector3(leftFist.transform.position.x, 0, 0);
-        leftFistY = new Vector3(0, leftFist.transform.position.y, 0);
+        rightFistCol.enabled = false;
+        leftFistCol.enabled = false;
+        isAttacking = false;
+        hasAttacked = false;
+        attackHit = false;
+        canAttackSprite.SetActive(false);
     }
 
     void Update()
     {
-        if (levelManager.gameHasStarted == true)
+        if (isAttacking == true)
         {
             attackTimer += Time.deltaTime;
+            canAttackSprite.SetActive(false);
         }
-        if (levelManager.gameHasStarted)
+        else
         {
-            if (attackTimer >= Random.Range(4, 6))
+            canAttackSprite.SetActive(true);
+        }
+
+        if (levelManager.gameHasStarted == true)
+        {
+            foreach (var playerController in _playersControllers)
             {
-                canAttack = true;
-                attackTimer = 0;
-            }
-
-            if (canAttack == true)
-            {
-                StartCoroutine(DecideFist());
-                canAttack = false;
+                if (playerController.PlayerIndex == 1)
+                {
+                    if (playerController.input.dodge == 1 && isAttacking == false)
+                    {
+                        Debug.Log("Left Punch");
+                        isAttacking = true;
+                        StartCoroutine(AttackLeft());
+                    }
+                    if (playerController.input.action == 1 && isAttacking == false)
+                    {
+                        Debug.Log("Right Punch");
+                        isAttacking = true;
+                        StartCoroutine(AttackRight());
+                    }
+                }
             }
         }
-    }
-
-    IEnumerator DecideFist()
-    {
-        randomValue = Random.value;
-
-        if (randomValue <= 0.5f)
-        {
-            leftAttack = true;
-            yield return StartCoroutine(AttackLeft());
-        }
-        else if (randomValue > 0.5f)
-        {
-            leftAttack = false;
-            yield return StartCoroutine(AttackRight());
-          }
     }
 
     IEnumerator AttackLeft()
     {
-        leftFist.transform.position = Vector3.Lerp(leftFistX, leftFistY + new Vector3(0, 0.4f, 0), Time.deltaTime * 3);
-        yield break;
+        yield return new WaitForSeconds(swingTime);
+        //ENEMY IS GOING TO ATTACK TEXT/PROMPT
+        leftFistCol.enabled = true;
+
+        if (attackHit == true)
+        {
+            levelManager.player1Score++;
+            levelManager.player2HP += 10;
+            leftFistCol.enabled = false;
+            attackTimer = 0;
+            hasAttacked = true;
+            isAttacking = false;
+            yield return attackHit = false;
+        }
+
+        if (attackTimer >= 2 && attackHit == false)
+        {
+            leftFistCol.enabled = false;
+            isAttacking = false;
+            yield return attackTimer = 0;
+        }
     }
 
     IEnumerator AttackRight()
     {
-        rightFist.transform.position = Vector3.Lerp(rightFistX, rightFistY + new Vector3(0, 0.4f, 0), 5);
-        yield break;
+        yield return new WaitForSeconds(swingTime);
+        //ENEMY IS GOING TO ATTACK TEXT/PROMPT
+        rightFistCol.enabled = true;
+
+        if (attackHit == true)
+        {
+            levelManager.player1Score++;
+            levelManager.player2HP += 10;
+            rightFistCol.enabled = false;
+            attackTimer = 0;
+            hasAttacked = true;
+            isAttacking = false;
+            yield return attackHit = false;
+        }
+
+        if (attackTimer >= 2 && attackHit == false)
+        {
+            rightFistCol.enabled = false;
+            isAttacking = false;
+            yield return attackTimer = 0;
+        }
     }
-    
 }
--- Assets/Scripts/KinecticRage/LevelManager.cs
+++ Assets/Scripts/KinecticRage/LevelManager.cs
@@ -1,24 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     public float gameTimer;
     public bool gameHasStarted;
+    public int player1Score;
+    public float player2HP;
+
+    public float decreaseP2HpValue;
+
+    public Slider enemySlider;
+    public TMPro.TMP_Text player1ScoreTxt;
 
     private void Start()
     {
+        player2HP = 100;
         gameHasStarted = false;
+
+        enemySlider.maxValue = 100;
+        enemySlider.minValue = 0;
     }
 
     private void Update()
     {
+        enemySlider.value = player2HP;
+        player1ScoreTxt.text = player1Score.ToString();
+
+        if (player2HP >= 100)
+        {
+            player2HP = 100;
+        }
         gameTimer += Time.deltaTime;
+        player2HP -= Time.deltaTime * decreaseP2HpValue;
 
         if (gameTimer >= 5 && gameHasStarted == false)
         {
             gameHasStarted = true;
         }
+
+        if (player1Score >= 20 || player2HP <= 0)
+        {
+            Debug.Log("Player1Wins");
+
+            //if time runs out player2wins
+        }
+
     }
 }
